b8c62de3997374771821edf88dc34ff8b61f2ae0
Lukas <lukas.slehofer@gmail.com>
2025-10-11 21:08:31 +0200

    Add Apple iCloud to supported calendar services on homepage
    - Add third card with Apple logo in 'Works With Your Favorite Calendar Services' section
    - Use black gradient background (gray-800 to black) matching CalDAV branding
    - Display Apple logo SVG icon in white
    - Show 'Apple iCloud' label
    - Now displays Google Calendar, Microsoft 365, and Apple iCloud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -255,6 +255,15 @@
                     </div>
                     <span class="text-xl font-semibold text-gray-900">{{ __('messages.microsoft_365') }}</span>
                 </div>
+                
+                <div class="flex items-center space-x-3">
+                    <div class="w-12 h-12 rounded-lg bg-gradient-to-r from-gray-800 to-black flex items-center justify-center">
+                        <svg class="w-7 h-7 text-white" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z"/>
+                        </svg>
+                    </div>
+                    <span class="text-xl font-semibold text-gray-900">Apple iCloud</span>
+                </div>
             </div>
         </div>
     </section>
